7cf9e420ad2ee87708f3213d7a13bbb89fae43e9
juyi_huang <juyi_huang@trend.com.tw>
2015-09-08 10:28:41 +0800

    update rpm_package recipe

--- opsworks_cookbooks/recipes/default.rb
+++ opsworks_cookbooks/recipes/default.rb
@@ -2,7 +2,7 @@ package 'git' do
   action :install
 end
 
-rpm_package "jenkins-1.619-1.1.noarch.rpm" do 
+rpm_package 'jenkins-1.619-1.1.noarch.rpm' do 
 	source :/home/test1/current
 	action :install
 end
