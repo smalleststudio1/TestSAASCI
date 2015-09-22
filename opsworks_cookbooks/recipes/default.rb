65dd8c44df75c2801ef8088e57369a3b43e0fdce
juyi_huang <juyi_huang@trend.com.tw>
2015-09-22 16:03:10 +0800

    update chef recipe

--- opsworks_cookbooks/recipes/default.rb
+++ opsworks_cookbooks/recipes/default.rb
@@ -2,11 +2,16 @@ package 'git' do
   action :install
 end
 
+remote_file "#{Chef::Config[:file_cache_path]}/jenkins-1.619-1.1.noarch.rpm" do
+    source "https://s3-us-west-2.amazonaws.com/testbuildpublish2/ETS/jenkins-1.619-1.1.noarch.rpm"
+    action :create
+	end 
+
 rpm_package 'jenkins-1.619-1.1.noarch.rpm' do
-    source "/home/test1/current/jenkins-1.619-1.1.noarch.rpm"
+    source "#{Chef::Config[:file_cache_path]}/jenkins-1.619-1.1.noarch.rpm"
 	action :install
 end
 
-service "jenkins" do 
+service 'jenkins' do 
    action :start
 end
